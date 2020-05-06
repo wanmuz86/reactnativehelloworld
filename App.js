3ea0721f5608ce700e653d9faad0512b9859bb86
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2020-05-06 21:59:36 +0800

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image,Button, TouchableHighlight } from 'react-native';
 
 export default function App() {
+  const handleBtn = () => alert('Hello World')
+
+  /* function handleButton {
+    alert('Hello World')
+  }*/
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Hello World</Text>
+      <Text style={styles.subHeading}>Welcome to my app</Text>
+      <Image 
+      source={require('./imgs/logo.png')} style={{width: 350, height: 200}}
+      />
+      <Button onPress={handleBtn} title="Press Me" />
+      <TouchableHighlight onPress={handleBtn}>
+        <View>
+    <Text>Press me!</Text>
+  </View>
+</TouchableHighlight>
     </View>
   );
 }
@@ -12,8 +27,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff0',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontSize:32,
+    color:'red'
+  },
+  subHeading:{
+    marginTop:10,
+    color:'#00f',
+    fontSize:24
+  }
+
 });
